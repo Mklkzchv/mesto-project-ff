ba9e5e005d7c07142c76b4f5931c28d16bb86d7d
Mikle <pirat750@mail.ru>
2024-04-13 01:17:29 +0300

    Feat: Add init cards and delete cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,8 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+
+
+
+//console.log(initialCards);
--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,44 @@
 
 // @todo: DOM узлы
 
+const config = {
+    cardTemplate: '#card-template',
+    cardElement: '.card',
+    deleteButton: '.card__delete-button',
+    cardImage: '.card__image',
+    cardTitle: '.card__title',
+    cardList: '.places__list'
+  };
+
+const placesList = document.querySelector(config.cardList);
+
 // @todo: Функция создания карточки
+
+function generateCard(card, deleteCard) {
+    const cardElement = document
+        .querySelector(config.cardTemplate)
+        .content
+        .querySelector(config.cardElement)
+        .cloneNode(true);
+
+    const deleteButton = cardElement.querySelector(config.deleteButton);
+    deleteButton.addEventListener('click', (evt) => {
+        deleteCard(evt); 
+    });
+    cardElement.querySelector(config.cardImage).src = card.link;
+    cardElement.querySelector(config.cardImage).alt = card.name;
+    cardElement.querySelector(config.cardTitle).textContent = card.name;
+    return cardElement
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(item) {
+    item.target.parentElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(card => {
+    placesList.append(generateCard(card, deleteCard));
+});
